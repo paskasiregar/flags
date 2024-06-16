853628055ac0d4d58b32b93e0b2d98ba7d1055f4
Debora Paskarina <paska@Paska-2.local>
2024-06-16 18:42:36 +0700

    add some colorings

--- src/components/countryCard.jsx
+++ src/components/countryCard.jsx
@@ -29,7 +29,7 @@ export const CountryCard = ({ item }) => {
         <p className="text-4xl">{item.flag}</p>
         <button
           onClick={handleDeleteCountry}
-          className="border-2 border-solid border-indigo-500 px-4 py-2 rounded-lg text-indigo-500 hover:border-rose-500 hover:bg-rose-100 hover:text-rose-500"
+          className="border-2 border-solid border-indigo-500 px-4 py-2 rounded-lg text-indigo-500 hover:border-rose-500 hover:bg-rose-50 hover:text-rose-500"
         >
           Delete
         </button>
